888b9ed917f2f88375b7b95c198c76f603d7b25d
Sam Kohler <samuko@gmx.com>
2018-04-18 15:48:17 -0400

    rough draft complete

--- srvctl.py
+++ srvctl.py
@@ -47,6 +47,8 @@ import sys
 import os
 import os.path
 import subprocess
+import time
+from datetime import datetime
 from subprocess import PIPE,Popen
 import re
 from datetime import datetime
@@ -87,33 +89,109 @@ EXAMPLES = '''
 
 
 # Global variables
-vcmd=""
-vobj=""
-vdb=""
-err_msg=""
+vcmd = ""
+vobj = ""
+vdb = ""
+err_msg = ""
+gi_home = ""
+ora_home = ""
+vchanged = ""
+nodenum = ""
+# Time to Wait (ttw) for Status in min
+myttw = 4
+
+def get_gihome():
+    """Determine the Grid Home directory"""
+
+    # gi_home=str(commands.getstatusoutput("dirname $( ps -eo args | grep ocssd.bin | grep -v grep | awk '{print $1}'")[1])
+    try:
+      process = subprocess.Popen(["dirname $( ps -eo args | grep ocssd.bin | grep -v grep | awk '{print $1}')"], stdout=PIPE, stderr=PIPE, shell=True)
+      output, code = process.communicate()
+    except:
+        err_msg = err_msg + ' get_gihome() retrieving GRID_HOME : (%s,%s)' % (sys.exc_info()[0],code)
+        module.fail_json(msg='ERROR: %s' % (err_msg), changed=False)
+
+    return(gi_home.strip())
+
+
+def get_node_num():
+    """Return current node number to ensure that srvctl is only executed on one node (1)"""
+    global gi_home
+
+    if gi_home is null:
+        get_gihome()
+
+    try:
+      process = subprocess.Popen([gi_home + "bin/olsnodes -l -n | awk '{ print $2 }'"], stdout=PIPE, stderr=PIPE, shell=True)
+      output, code = process.communicate()
+    except:
+        err_msg = err_msg + ' get_gihome() retrieving GRID_HOME : (%s,%s)' % (sys.exc_info()[0],code)
+        module.fail_json(msg='ERROR: %s' % (err_msg), changed=False)
+
+    return(nodenum.strip())
+
 
 def get_orahome(vdb):
     """Return database home as recorded in /etc/oratab"""
+    global ora_home
 
     try:
-        v_orahome=str(commands.getstatusoutput("cat /etc/oratab | grep -m 1 tstdb | grep -o -P '(?<=:).*(?<=:)' |  sed 's/\:$//g'")[1])
+        process = subprocess.Popen(["cat /etc/oratab | grep -m 1 tstdb | grep -o -P '(?<=:).*(?<=:)' |  sed 's/\:$//g'"], stdout=PIPE, stderr=PIPE, shell=True)
+        output, code = process.communicate()
     except:
-        err_msg = err_msg + ' get_orahome() retrieving ORACLE_HOME from /etc/oratab : (%s)' % (sys.exc_info()[0])
+        err_msg = err_msg + ' get_orahome() retrieving ORACLE_HOME from /etc/oratab : (%s,%s)' % (sys.exc_info()[0],code)
         module.fail_json(msg='ERROR: %s' % (err_msg), changed=False)
 
-    return(v_orahome)
+    ora_home = output.strip()
+    return(ora_home)
 
 
-def get_db_status(vdb, vhome, vstatus):
-    """Return the status of the database"""
+def get_db_status(vdb):
+    """Return the status of the database on every node"""
+    dbStatus = {}
 
+    vgihome = get_gihome()
 
     try:
       # for Python3 look at subprocess.check_output
-      t_status=str(commands.getstatusoutput("export ORACLE_HOME=" + vohome + "; export ORACLE_SID=" + vdb + "1 ; " + vhome + "/bin/srvctl status database -d " + vdb + " -v | grep -o ': .*' | cut -f2- -d: | sed 's/\.$//g'"")[1])
+      # t_status=str(commands.getstatusoutput(vgihome + "/bin/crsctl status resource ora." + vdb + ".db | grep STATE")[1])
+      process = subprocess.Popen([ vgihome + "/bin/crsctl status resource ora." + vdb + ".db | grep STATE"], stdout=PIPE, stderr=PIPE, shell=True)
+      output, code = process.communicate()
     except:
-        err_msg = err_msg + ' Error: spfile_bu() retrieving spfiile bu summary : (%s)' % (sys.exc_info()[0])
+        err_msg = err_msg + ' Error: get_db_status() : (%s,%s)' % (sys.exc_info()[0],code)
+        module.fail_json(msg='ERROR: %s' % (err_msg), changed=False)
 
+    node_status=output.strip().split(",")             #  ['STATE=OFFLINE', ' OFFLINE']
+    node_status[1]=node_status[1].strip()       # removes space in front of " OFFLINE"
+    node_status[0]=node_status[0].split("=")[1] # splits STATE and OFFLINE and returns OFFLINE
+    i = 0
+    while i < len(node_status):
+        dbStatus[i]=node_status[i]
+        i += 1
+
+    return(dbStatus)
+
+
+def wait_for_status(vnodes,vstatus):
+    """Compare database status of both nodes to expected status. Loop in 5 second intervals until state obtained"""
+    vduration =  time.time() + datetime.timedelta(minutes=myttws)
+    nodematches = [False] * len(vnodes) # Each node assumed not to match the status we're looking for (vstatus)
+    oneholdout = True   # as long as there is one node that doesn't match oneholdout=True
+
+    # until all nodes match the status, or we reach duration start time plus ttw defined a the top
+    while oneholdout and datetime.now() < vduration:
+        i = 0
+        for i < len(vnodes):
+            if vnodes[i] != vstatus:
+                nodematches[i] = False
+            else:
+                nodematches[i] = True
+
+        # built in all() checks for all True
+        if all(nodematches):
+            oneholdout = False
+        else:
+            time.sleep(3)
 
 
 # ==============================================================================
@@ -121,6 +199,9 @@ def get_db_status(vdb, vhome, vstatus):
 # ==============================================================================
 def main ():
     """ Execute srvctl commands """
+    global gi_home
+    global ora_home
+    global nodenum
 
     ansible_facts={}
 
@@ -151,13 +232,41 @@ def main ():
         vopt = '-i'
         vdb = vdb + "1"
 
-    vdbhome = get_orahome(vdb)
+    if vcmd is 'stop':
+        exp_status="OFFLINE"
+    elif vcmd is 'start':
+        exp_status="ONLINE"
+
+    gi_home = get_gihome(vdb)
+    ora_home = get_orahome(vdb)
+    nodenum = get_node_num()
 
     # before executing a command get the db status
-    get_db_status(vdb, vdbhome)
+    current_status = get_db_status(vdb)
 
-    # Execute the command
-    try:
-      temp = str(commands.getstatusoutput("export ORACLE_HOME=" + vdbhome + "; " + vdbhome + "/bin/srvctl " + vcmd + " " + vobj + " " + vopt + " " + vdb)[1])
-    except:
-      err_msg = err_msg + ' Error: srvctl : cmd %s vobj %s db %s opt %s sysinfo: %s' % (vcmd, vobj, vdb, vopt, sys.exc_info()[0])
+    if current_status != exp_sstatus:
+        # Execute the srvctl command
+        try:
+            temp = str(commands.getstatusoutput("export ORACLE_SID=" + vdb + nodenum "; export ORACLE_HOME=" + ora_home + "; " + vdbhome + "/bin/srvctl " + vcmd + " " + vobj + " " + vopt + " " + vdb)[1])
+        except:
+            err_msg = err_msg + ' Error: srvctl module : cmd %s vobj %s db %s opt %s sysinfo: %s' % (vcmd, vobj, vdb, vopt, sys.exc_info()[0])
+            module.fail_json(msg='ERROR: %s' % (err_msg), changed=False)
+
+        # Once the command is executed wait for the proper state
+        wait_for_status(vdb, exp_status)
+
+        if vcmd == "start":
+            vcmd = "started"
+        elif vcmd == "stop":
+            vcmd = "stopped"
+
+        msg = "srvctl module complete. Database: " + vdb + " " + vcmd + "."
+        vchanged = "True"
+
+    else:
+
+        msg = "database already " + current_status + " no action taken."
+        vchanged = "False"
+
+
+    module.exit_json( msg=msg, ansible_facts=ansible_facts , changed=vchanged)
