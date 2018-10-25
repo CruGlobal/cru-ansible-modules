e41a287e0ce8f872c6e3d8e4e2c1ae0724ceff99
Sam Kohler <samuko@gmx.com>
2018-10-25 13:50:28 -0400

    commiting currednt oramemck.py for Friday remote Oct 25 2018

--- oramemck.py
+++ oramemck.py
@@ -21,6 +21,12 @@ node_number = ""
 debug_msg = ""
 debugme = True
 vphys_mem = ""
+# defaults:
+default_sga_target_amt = 2000
+default_sga_target_units = "mb"
+default_pga_aggregate_target_amt = 1000
+default_pga_aggregate_target_units = "mb"
+
 
 ANSIBLE_METADATA = {'status': ['stableinterface'],
                     'supported_by': 'Cru DBA team',
@@ -28,33 +34,69 @@ ANSIBLE_METADATA = {'status': ['stableinterface'],
 
 DOCUMENTATION = '''
 ---
-module: memck ( memory check )
-short_description: This memory determines if there is enough memory on the destination
-    host to add and start up another database. If not it attempts to adjust the
-    sga and pga settings so that the database will start and fit in available memory.
+module: oramemck ( Oracle memory check - HugePages )
 
-notes: If the pfile location is provided this module will adjust the sga_target and
-    pga_aggregate_target accordingly.
+short_description:
+    This module determines if there is enough memory on the destination server
+    to start up another database. If not it attempts to adjust the sga and pga settings
+    so that the database will start and fit in available memory.
+        1. Will check source sga_target and pga_aggregate_target to see if it will fit.
+        2. If source ( sga_target and pga_aggregate_target ) amounts are too big it will check predefined amounts
+           against available size on the destination server and programmed defaults * (sga_target: 2000MB and pga_aggregate_target: 1000MB)
+           * These programmed default amounts are what where required to stand up a restore of SBLCRMP.
+
+notes:
+    If the pfile location is provided this module will adjust the sga_target and
+    pga_aggregate_target in the pfile.
+
 '''
 
 EXAMPLES = '''
 
-    # if cloning or restoring a database place this call just before the
+    # If cloning or restoring a database place this call just before the
     # 'startup new database with pfile' task.
     # Argument units should be (KB) and/or add the size units to the argument.
-    # i.e. '2040 MB' or '2048 mb' either way 1024 base is assumed.
+    # i.e. '2040 MB' or '2048 mb' either way 1024 is assumed for calculations.
     - name: Check memory on destination host
-      memck:
-        src_sga_tgt: "{{ sourcefacts['sga_target'] }}"
-        src_pga_agg_tots: "{{ sourcefacts['pga_aggregate_target'] }}"
-        dest_mem_info: "{{ orafacts['hugepages'] }}"
+      oramemck:
+        src_sga_target: "{{ sourcefacts['sga_target'] }}"
+        src_sga_max_size: "{{ sourcefacts['sga_max_size']}}"
+        src_pga_aggregate_target: "{{ sourcefacts['pga_aggregate_target'] }}"
+        src_memory_target: "{{ sourcefacts['memory_target']}}"
+        src_memory_max_target: "{{ sourcefacts['memory_max_target']}}"
+        src_use_large_pages: "{{ sourcefacts['use_large_pages']}}"
+        dest_sga_target: "{{ database_parameters[dest_db_name].sga_target }}"
+        dest_pga_aggregate_target: "{{ database_parameters[dest_db_name].pga_aggregate_target }}"
         pfile: "{{ oracle_stage }}/pfile.ora"
 
     notes:
         pfile: is optional. If given the module will attempt to update
-        all occurrances of sga_target and __pga_aggregate_target
+        all occurrances of sga_target and pga_aggregate_target
 
 '''
+
+
+def split_amt_n_long_units(vinput):
+    """Take a string input of format '2000M' or '2000MB' and convert to '2000' and 'MB' and return amount '2000' and units (k,m,g) in long format 'mb' """
+    j = 0
+    for vchar in reversed(vinput):
+        if vchar.isdigit():
+            print "vchar: %s is a digit. vcount: %s" % (vchar,j)
+            tmp_units = vinput[-j:]
+            idx = len(vinput) - j
+            tmp_amt = vinput[:idx]
+            break
+        j += 1
+
+    if len(tmp_units) == 1:
+        if tmp_units.lower() = "k":
+            tmp_units = "kb"
+        elif tmp_units.lower() = "m":
+            tmp_units = "mb"
+        elif tmp_units.lower() = "g":
+            tmp_units = "gb"
+
+    return tmp_amt,tmp_units
 
 
 def get_gihome():
@@ -124,7 +166,7 @@ def shared_mem_in_use():
 
 def hugepages():
     """Gather Hugepage information from the host including database parameters for all running databases."""
-    # Example of what this function returns:  https://www.kernel.org/doc/Documentation/vm/hugetlbpage.txt
+    # Example of the dictionary this function returns:  https://www.kernel.org/doc/Documentation/vm/hugetlbpage.txt
     # --------------------------------------
     #     "hugepages": {
     #     "meminfo": {
@@ -168,7 +210,7 @@ def hugepages():
 
     # get system memory information
     try:
-        cmd_str = "/bin/grep 'Huge\|Mem\|Swap' /proc/meminfo | grep -v Cached"
+        cmd_str = "/bin/grep 'Huge\|Mem\|Swap' /proc/meminfo | /bin/grep -v Cached"
         process = subprocess.Popen([cmd_str], stdout=PIPE, stderr=PIPE, shell=True)
         output, code = process.communicate()
     except:
@@ -189,7 +231,7 @@ def hugepages():
 
     # server configuration setting for number of hugepages in Huge Pages pool
     try:
-        cmd_str = "/bin/cat /etc/sysctl.conf | /bin/grep huge | cut -d '=' -f2"
+        cmd_str = "/bin/cat /etc/sysctl.conf | /bin/grep huge | /bin/cut -d '=' -f2"
         process = subprocess.Popen([cmd_str], stdout=PIPE, stderr=PIPE, shell=True)
         output, code = process.communicate()
     except:
@@ -217,7 +259,7 @@ def hugepages():
 
     # soft and hard memlock
     try:
-        cmd_str = "/bin/cat /etc/security/limits.conf | grep memlock | grep -v '#'"
+        cmd_str = "/bin/cat /etc/security/limits.conf | /bin/grep memlock | /bin/grep -v '#'"
         process = subprocess.Popen([cmd_str], stdout=PIPE, stderr=PIPE, shell=True)
         output, code = process.communicate()
     except:
@@ -258,7 +300,7 @@ def hugepages():
 
 
 def convert_to(vsize_amt,vsize_unit_now,vsize_unit_to):
-    """Convert input to units passed"""
+    """Input arguments 1: amount, 2 current_unit, 3 convert to units. Pass back new amt and unit"""
 
     if int(vsize_amt) == 0:
         return vsize_amt,vsize_unit_to
@@ -332,7 +374,9 @@ def get_db_home(local_db):
 
 
 def get_pga_sga_totals():
-    """Get a list of running databases on the host and select all sga_target_totals, sum and return the sum of all running dbs."""
+    """Get a list of running databases on the host and select sga_target and pga_aggregate_target from each db while keeping a running total.
+       Returning python dictionary object : { 'sga_totals': sga_totals, 'sga_units': 'kb', 'pga_totals': pga_totals, 'pga_units': 'kb' }"""
+
     db_param = ["sga_target","pga_aggregate_target"]
     sga_totals = 0
     pga_totals = 0
@@ -381,12 +425,14 @@ def get_pga_sga_totals():
     return(fx_info)
 
 
-def recommended_hugepages(hugepg_size,hugepg_size_unit):
-    """(Doc ID 401749.1) Compute values for the recommended HugePages/HugeTLB configuration for the current shared memory segments on Oracle Linux."""
+def number_recommended_hugepages(hugepg_size,hugepg_size_unit):
+    """(Doc ID 401749.1) Compute values for the recommended HugePages/HugeTLB configuration for the current shared memory segments on Oracle Linux.
+       Input arg1 string HugePage size (amount) and arg2 string units (kb).
+       If kernel version is correct (2.4, 2.6, 3.8, 3.10, 4.1) the function returns number of recommended HugePages."""
 
     # get kernel version (i.e. 2.6)
     try:
-        cmd_str = "uname -r | uname -r | grep -Po '^...'"
+        cmd_str = "/bin/uname -r | /bin/grep -Po '^...'"
         process = subprocess.Popen(cmd_str, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output, code = process.communicate(cmd_str)
     except:
@@ -397,8 +443,9 @@ def recommended_hugepages(hugepg_size,hugepg_size_unit):
     vkern = output.strip()
 
     # Get a sum total of shared memory segements currently running on the host
+    # This is a moving target so don't take the value as hard.
     try:
-        cmd_str = "ipcs -m|awk '{ print $5}'|awk '{a+=$0}END{print a}'"
+        cmd_str = "ipcs -m|awk '{ print $5}'| /bin/awk '{a+=$0}END{print a}'"
         process = subprocess.Popen(cmd_str, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output, code = process.communicate(cmd_str)
     except:
@@ -409,15 +456,15 @@ def recommended_hugepages(hugepg_size,hugepg_size_unit):
     # Shared memory being used (bytes)
     vrunning_shared_mem = output.strip()
 
-    # Convert shared memory being used (bytes) to kb:
+    # Convert shared memory being used (bytes) to kb:                            from    to
     vrun_shared_mem_size,vrun_shared_mem_units = convert_to(vrunning_shared_mem,"bytes","kb")
 
-    # make sure hugepage_size is in kb: convert_to(vsize_amt,vsize_unit_now,vsize_unit_to):
+    # make sure arguments passed in: hugepage_size is in kb: convert_to(vsize_amt,vsize_unit_now,vsize_unit_to):
     if hugepg_size_unit.lower() != "kb":
         hugepg_size,hugepg_unit = convert_to(hugepage_size,hugepg_unit,"kb")
 
     # number of pages required to handle the currently running shared memory segments, Oracle and otherwise.
-    # shared_memory (kb) / HugePage_size (kb) = total required number of pages
+    # total required number of pages = shared_memory (kb) / HugePage_size (kb)
     req_pgs = int(vrunning_shared_mem) / int(hugepg_size)
 
     # return recommendation for HugePage pool size based on kernel version
@@ -449,79 +496,106 @@ def main ():
 
     module = AnsibleModule(
       argument_spec = dict(
-        src_sga_target              =dict(required=True),
-        src_sga_max_size            =dict(required=True),
-        src_pga_aggregate_target    =dict(required=True),
-        src_memory_target           =dict(required=True),
-        src_memory_max_target       =dict(required=True),
-        src_use_large_pages         =dict(required=True),
-        pfile                       =dict(required=True)
+        src_sga_target                     =dict(required=True),
+        src_sga_max_size                   =dict(required=True),
+        src_pga_aggregate_target           =dict(required=True),
+        src_memory_target                  =dict(required=True),
+        src_memory_max_target              =dict(required=True),
+        src_use_large_pages                =dict(required=True),
+        pre_defined_sga_target             =dict(required=True),
+        pre_defined_pga_aggregate_target   =dict(required=True),
+        pfile                              =dict(required=True)
       ),
       supports_check_mode=True,
     )
 
     # Get arguements passed from Ansible playbook
-    vsrc_sga_target             = module.params.get('src_sga_target')
-    vsrc_sga_max_size           = module.params.get('sga_max_size')
-    vsrc_pga_aggregate_target   = module.params.get('src_pga_aggregate_target')
-    vsrc_memory_target          = module.params.get('src_memory_target')
-    vsrc_memory_max_target      = module.params.get('src_memory_max_target')
-    vsrc_use_large_pages        = module.params.get('src_use_large_pages')
-    vpfile                      = module.params.get('pfile')
+    vsrc_sga_target                    = module.params.get('src_sga_target')
+    vsrc_sga_max_size                  = module.params.get('sga_max_size')
+    vsrc_pga_aggregate_target          = module.params.get('src_pga_aggregate_target')
+    vsrc_memory_target                 = module.params.get('src_memory_target')
+    vsrc_memory_max_target             = module.params.get('src_memory_max_target')
+    vsrc_use_large_pages               = module.params.get('src_use_large_pages')
+    vpre_defined_sga_target            = module.params.get('pre_defined_sga_target')
+    vpre_defined_pga_aggregate_target  = module.params.get('pre_defined_pga_aggregate_target')
+    vpfile                             = module.params.get('pfile')
 
-    # You cannot be using AMM to use hugepages. Disable AMM by setting MEMORY_TARGET and MEMORY_MAX_TARGET should be set to 0
+    # You cannot use AMM with hugepages.
+    # Disable AMM by setting MEMORY_TARGET and MEMORY_MAX_TARGET to 0
     if vsrc_memory_target != 0 and vsrc_memory_max_target != 0:
         tmp_str = "Automatic Memory Management (AMM) CANNOT be used with hugepages. To disable AMM set MEMORY_TARGET and MEMORY_MAX_TARGET to 0."
         msgg(tmp_str)
 
-
+    # Call hugepages() function and get server hugepage info
     tmp = hugepages()
     vmeminfo = tmp['hugepages']['meminfo']
     # available hugepages = Total -  Reserved
-    vphys_mem       = vmeminfo['MemTotal']
-    vhuge_free      = vmeminfo['HugePages_Free']
-    vhuge_rsvd      = vmeminfo['HugePages_Rsvd']
-    vhuge_tot       = vmeminfo['HugePages_Total']
-    tmp_huge_size   = vmeminfo['Hugepagesize']      # kb
-    vmemlock_hard   = vmeminfo['memlock']['hard']   # kb
-    vmemlock_soft   = vmeminfo['memlock']['soft']   # kb
+    vphys_mem       = vmeminfo['MemTotal']          # actual physical memory on the server
+    vhuge_free      = vmeminfo['HugePages_Free']    # number of free HugePages - number of unused HugePages
+    vhuge_rsvd      = vmeminfo['HugePages_Rsvd']    # number of reserved HugePages - not yet allocated ( not yet subtracted from 'free', but reserved for use.)
+    vhuge_tot       = vmeminfo['HugePages_Total']   # number of total HugePages
+    tmp_huge_size   = vmeminfo['Hugepagesize']      # HugePage mem size (kb)
+    vmemlock_hard   = vmeminfo['memlock']['hard']   # Memlock_hard (kb)
+    vmemlock_soft   = vmeminfo['memlock']['soft']   # Memlock_soft (kb)
 
-    # trim units off the end of hugepagesize '2048 kB'
-    vhuge_size,vhuge_size_units = tmp_huge_size.split()
+    # split tmp_huge_size string into amount (size) and units (kb) e.x. tmp_huge_size : '2048 kB'
+    vhuge_size_amt,vhuge_size_units = tmp_huge_size.split()
     # if hugepage_size is not kb convert to kb:
-    if vhuge_size_units != "kb":
-        vhuge_size,vhuge_size_units = convert_to(vhuge_size,vhuge_size_units)
+    if vhuge_size_units.lower() != "kb":
+        vhuge_size_amt,vhuge_size_units = convert_to(vhuge_size_amt,vhuge_size_units,"kb")
 
-    # Check that memlock.soft and memlock.hard are equal as they should have been set that way
+    # Check that memlock.soft and memlock.hard are equal ( they should have originally been set that way )
     if vmemlock_hard != vmemlock_soft:
-        msgg "The hard memlock %s and soft memlock %s are note equal, but should be." % (vmemlock_hard,vmemlock_soft)
+        msgg "The hard memlock %s and soft memlock %s are note equal, but should be. Change it here: %s" % (vmemlock_hard,vmemlock_soft,"/proc/meminfo")
 
-    # Use oracle script to get recommended hugepages size for currently running shared memory
-    vrecom_num_hgpgs = recommended_hugepages(vhuge_size,vhuge_size_units)
-    if vrecom_hgpgs > vhuge_tot:
-        msgg "NOTICE: Recommended HugePages (currently recommended to accomodate running shared memory): %s exceeds currently configured total number of HugePages: %s" % (vrecom_hgpgs,vhuge_tot)
+    # Use Oracle provided script to get recommended hugepages size for currently running shared memory
+    vrecomm_num_hgpgs = number_recommended_hugepages(vhuge_size_amt,vhuge_size_units)
 
-    # Get sga_target and pga_aggregate_target totals ( summation of all databases parameters )
-    # fx returns dict { "sga_totals": sga_totals, "sga_units": "kb", "pga_totals": pga_totals, "pga_units": "kb" }
+    # Simple compare of recommended total to current HugePage total
+    if int(vrecomm_num_hgpgs) > int(vhuge_tot):
+        msgg "NOTICE: Recommended HugePages (currently recommended to accomodate running shared memory): [%s] exceeds currently configured total number of HugePages: [%s]" % (vrecom_hgpgs,vhuge_tot)
+    else:
+        msgg "Current HugePages: [%s] and recommended HugePages based on currently running processes: [%s]" % (vhuge_tot,vrecomm_num_hgpgs)
+
+    # Get summation of sga_target and pga_aggregate_target totals ( summation of all databases on the host )
+    # fx returns a dictionary of format: { "sga_totals": sga_totals, "sga_units": "kb", "pga_totals": pga_totals, "pga_units": "kb" }
+    # This works in parallel (another size / space check) along with Oracle script that calculates recommended number of HugePages.
     sga_pga_totals = get_pga_sga_totals()
 
-    # Free/unused hugepages: number of Free HugePages - Reserved HugePages ( Reserved HugePages come out of the Free HugePages )
-    vnum_free_unall_hgpgs = int(vhuge_free) - int(vhuge_rsvd)
+    # Free or unused unallocated (reserved) hugepages: number of Free HugePages - Reserved HugePages ( Reserved HugePages will come out of the Free HugePages )
+    # so 'free' hugepages do not reflect true free HugePages unless reserved = 0
+    vnum_free_unused_unallocated_hgpgs = int(vhuge_free) - int(vhuge_rsvd)
 
-    # Number of pages required for the new database
-    src_db_req_hg_pgs = int(vsrc_sga_target) / int(huge_size)
+    # Number of HugePages required for the new database using source db sga
+    src_req_hgpgs = int(vsrc_sga_target) / int(huge_size)
 
-    # get total physical memory required by the source database
-    src_phys_mem_req = int(src_sga_tgt) + int(src_pga_agg_tots)
+    # get total physical memory required by the source database (kb) = sga_target + pga_aggregate_target
+    src_phys_mem_req = int(vsrc_sga_target) + int(vsrc_pga_aggregate_target)
 
-    # Check if number of HugePages required for the new database (src_db_req_hg_pgs) is smaller than unused/unallocated HuagePages (vnum_free_unall_hgpgs)
-    # Check that sga_target of new db + pga_aggregate_target of new db is less than free physical memory
+    # Check if the source sizes are too big to fit on the new server
+    # Check if number of HugePages required for the new database (src_db_req_hg_pgs) is less than truly free HugePages,
+    # which is free HugePages minus reserved HugePages (vnum_free_unused_unallocated_hgpgs)
+    # Check that sga_target + pga_aggregate_target ( src_phys_mem_req ) of new db is less than free physical memory
     # if so, we're good to go. No further action required.
-    if ( int(src_db_req_hg_pgs) > int(vnum_free_unall_hgpgs) ) or ( int(src_phys_mem_req) > int(vnum_free_unall_hgpgs) ):
+    # So if either of the blow conditions is true, there's a problem.
+    if ( int(src_db_req_hg_pgs) > int(vnum_free_unused_unallocated_hgpgs) ) or ( int(src_phys_mem_req) > int(vnum_free_unall_hgpgs) ):
 
-        # If either of the above is true, attempt to calculate parameter values for sga_target and pga_aggregate_target hat will work with space available.
+        # If either of the above is true there's a space problem, see if the defined parameter amounts will fit.
+        # Take units off the end of vdest_sga_target being passed in
+        # and convert to kb
+        if not vdest_sga_target[-1].isdigit():
+            vdest_sga_target_amt,vdest_sga_target_unit = split_amt_n_long_units(vdest_sga_target)
+            if vdest_sga_target_unit.lower() != "kb":
+                vdest_sga_target_amt,vdest_sga_target_unit = convert_to(vdest_sga_target_amt,vdest_sga_target_unit,"kb")
 
+        # Take units off the end of vdest_pga_aggregate_target
+        # and convert to kb
+        if not vdest_pga_aggregate_target[-1].isdigit():
+            vdest_pga_aggregate_target_amt,vdest_pga_aggregate_target_unit = set_long_unit(vdest_pga_aggregate_target)
+            if vdest_pga_aggregate_target_unit.lower() != "kb":
+                vdest_pga_aggregate_target_amt,vdest_pga_aggregate_target_unit = convert_to(vdest_pga_aggregate_target_amt,vdest_pga_aggregate_target_unit,"kb")
 
+        # check parameter passed sizes against module default sizes and server space.
 
 
 
