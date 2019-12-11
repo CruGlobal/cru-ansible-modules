e1f6d98a0d8d961978978934102f12104828bcab
Sam Kohler <samuko@gmx.com>
2019-12-11 09:24:40 -0500

    fixed error where 11g db used compatible value to construct oracle_home but used too many digits

--- dbfacts.py
+++ dbfacts.py
@@ -670,8 +670,13 @@ def main ():
 
         # We know there's no easy way like 12 to get database home internally for 11 so set 11 manually
         if "11" in ansible_facts[refname]['compatible']:
-            home = '/app/oracle/%s/%s' %  ( ansible_facts[refname]['compatible'], db_home_name)
+            comp = ""
+            db_home = ""
+            comp = ansible_facts[refname]['compatible']
+            db_home = comp[:-(len(comp.split(".")[4])+1):]
+            home = '/app/oracle/%s/%s' %  ( db_home, db_home_name)
             debugg("-------- > home=%s" % (home))
+            # this splits compatible 11.2.0.4.0 then takes len of the last item adds 1 for the decimal '-' drops the last two chars
             ansible_facts[refname].update( { 'oracle_home' : home } )
 
         # See if dbainfo user/schema exists
