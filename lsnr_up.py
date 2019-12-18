336ae74305f812ed0ab3bc86f5012a793d3aae2d
Sam Kohler <samuko@gmx.com>
2019-12-18 09:00:04 -0500

    debugging fixes. Fix issues where debugging was being written to msg even when debug=False

--- lsnr_up.py
+++ lsnr_up.py
@@ -349,7 +349,7 @@ def main ():
         while (int(current_count) < int(v_entries)) and (time.time() < timeout):
             time.sleep(int(sleep_time))
             current_count = num_listeners(vdb)
-            msg = msg + " current_count: %s time.time() %s <= timeout: %s diff %s" % (current_count,time.time(),timeout,(timeout - time.time()))
+            debugg(" current_count: %s time.time() %s <= timeout: %s diff %s" % (current_count,time.time(),timeout,(timeout - time.time())) )
     except:
         custom_err_msg = 'Error[ lsnr_wait() ]: waiting for %s database to register with lsnrctl. current_count %s <= expected_num_reg_lsnrs %s and time.time() %s < timeout %s oracle_home %s msg: %s' % (vdb,current_count,expected_num_reg_lsnrs,time.time(),timeout,oracle_home,msg)
         custom_err_msg = custom_err_msg + "%s, %s, %s" % (sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
